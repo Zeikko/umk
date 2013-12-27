ef6f540fb46f759d9a1a431e5b8b566dd73afaec
jaakko <jaakko@jaakko-Latitude-E6530>
2013-12-27 13:30:18 +0200

    Added a link to the artist picture.

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,29 +1,27 @@
 <div id="artists">
     <?php
-    $i = 0;
     foreach ($artists as $artist) {
-
         ?>
-            <div class="artist col-md-3">
-                <h2><a href="<?php echo $this->createUrl('site/artist', array('name' => urlencode($artist['name']))) ?>"><?php echo $artist['name']; ?></a></h2>
-                <div class="image-wrapper">
+        <div class="artist col-md-3">
+            <h2><a href="<?php echo $this->createUrl('site/artist', array('name' => urlencode($artist['name']))) ?>"><?php echo $artist['name']; ?></a></h2>
+            <div class="image-wrapper">
+                <a href="<?php echo $this->createUrl('site/artist', array('name' => urlencode($artist['name']))) ?>">
                     <img class="img-rounded" src="<?php echo Artist::getImage($artist['name']); ?>" alt="<?php echo $artist['name']; ?>" />
-                </div>
-                <?php
-                $this->widget('TimeseriesChart', array(
-                    'url' => Yii::app()->params['tweetCounterUrl'] . 'groups/tweets/',
-                    'serieName' => 'Tweettejä',
-                    'parameters' => array(
-                        'group' => $artist['name'],
-                        'from' => date('c', ceil(strtotime('-7 days') / 86400) * 86400),
-                        'to' => date('c', ceil(time() / 86400) * 86400),
-                    )
-                ));
-                ?>
+                </a>
             </div>
             <?php
-
-        $i++;
+            $this->widget('TimeseriesChart', array(
+                'url' => Yii::app()->params['tweetCounterUrl'] . 'groups/tweets/',
+                'serieName' => 'Tweettejä',
+                'parameters' => array(
+                    'group' => $artist['name'],
+                    'from' => date('c', ceil(strtotime('-7 days') / 86400) * 86400),
+                    'to' => date('c', ceil(time() / 86400) * 86400),
+                )
+            ));
+            ?>
+        </div>
+        <?php
     }
     ?>
 </div>
